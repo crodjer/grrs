bd3ac676142ff183213f47ffcbdc92adff68568e
Rohan Jain <crodjer@gmail.com>
2019-01-06 11:53:27 +0530

    Generate iterator instead of vector as matches

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-// TODO: Genrate an iterator instead of Vec
-pub fn find_matches<I>(lines: I, pattern: &str) -> Vec<String>
+pub fn find_matches<'a, I: 'a>(lines: I, pattern: &'a str) -> impl Iterator<Item = String> + 'a
 where
     I: Iterator<Item = String>,
 {
-    lines.filter(|line| line.contains(&pattern)).collect()
+    lines.filter(move |line| line.contains(pattern))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_matches;
+
+    #[test]
+    fn test_find_matches() {
+        let lines = ["A test", "Actual content", "More content", "Another test"]
+            .iter()
+            .map(|s| s.to_string());
+        let results: Vec<_> = find_matches(lines, "test").collect();
+        assert_eq!(results, ["A test", "Another test"]);
+    }
 }
